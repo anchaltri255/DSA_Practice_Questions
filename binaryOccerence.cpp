4e56afc2fb97d5244b7e5468f8998962086cdeb8
Anchal Tripathi <anchaltripathi@gmail.com>
2024-04-05 14:18:19 +0530

    Add binaryOccerence.cpp in my repo

---
+++ binaryOccerence.cpp
@@ -0,0 +1,57 @@
+#include<iostream>
+
+using namespace std;
+
+int firstOcc(int arr[], int n, int key){
+	int s=0, e=n-1;
+	int mid = s+(e-s)/2;
+	int ans =-1;
+	
+	while(s<=e){
+		if(arr[mid] == key){
+		    ans = mid;
+			e = mid-1;
+		}
+		else if(key>arr[mid]){
+			s=mid+1;
+		}
+		else if(key<arr[mid]){
+			e = mid-1;
+		}
+		mid = s+(e-s)/2;
+	}
+	return ans;
+}
+
+int lastOcc(int arr[], int n, int key){
+	int s=0, e=n-1;
+	int mid = s+(e-s)/2;
+	int ans =-1;
+	
+	while(s<=e){
+		if(arr[mid] == key){
+			ans = mid;
+			s = mid+1;
+		}
+		else if(key>arr[mid]){
+			s=mid+1;
+		}
+		else if(key<arr[mid]){
+			e = mid-1;
+		}
+		mid = s+(e-s)/2;
+	}
+	return ans;
+}
+
+int main(){
+	int even[5] = {1,2,3,3,5};
+	
+	cout<<"First Occurrence of 3 at Index "<<firstOcc(even, 5, 3)<<endl;
+	cout<<"Last Occurrence of 3 at Index "<<lastOcc(even, 5, 3)<<endl;
+	cout<<"Total no of occurrence "<<(lastOcc(even, 5, 3)-firstOcc(even, 5, 3))+1<<endl;
+	
+	return 0;
+	
+}
+
